8484ca9f7f7adf3b05ffef2cb22941fe46413fd9
iuewcqm <capricorn.k@yandex.ru>
2023-02-15 15:44:11 +0300

    just good and easy challenge

--- 4kyu/twice_linear.hs
+++ 4kyu/twice_linear.hs
@@ -1,16 +1,17 @@
-import Data.List (sort, nub)
+dblLinear :: Int -> Integer
+dblLinear = (u !!)
 
-dblLinear :: Int -> [Integer]
-dblLinear n = take (2*n) $ u y z
-
-y = [2*x+1 | x <- [1..]]
-z = [3*x+1 | x <- [1..]]
-u yss@(y:ys) zss@(z:zs)
-  | y == z    = y: u ys zs
-  | y < z     = y: u ys zss
-  | otherwise = z: u yss zs 
-
+y = [2*x+1 | x <- u]
+z = [3*x+1 | x <- u]
+u = 1 : u' y z
+  where u' yss@(y:ys) zss@(z:zs)
+          | y > z     = z : u' yss zs
+          | y < z     = y : u' ys zss
+          | otherwise = y : u' ys zs
 
 main :: IO()
 main = do
-  print $ dblLinear 10
+  print $ dblLinear 10 -- 22
+  print $ dblLinear 20 -- 57
+  print $ dblLinear 30 -- 91
+  print $ dblLinear 50 -- 175
