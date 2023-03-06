a7b623fcf984a85f59cf5a4780e3853924588aa2
iuewcqm <capricorn.k@yandex.ru>
2023-03-06 19:21:57 +0300

    some easy challenge

---
+++ 4kyu/simplistic_TCP_FSM.py
@@ -0,0 +1,34 @@
+# https://www.codewars.com/kata/54acc128329e634e9a000362
+
+automata = {
+    ("CLOSED", "APP_PASSIVE_OPEN") : "LISTEN",
+    ("CLOSED", "APP_ACTIVE_OPEN")  : "SYN_SENT",
+    ("LISTEN", "RCV_SYN")          : "SYN_RCVD",
+    ("LISTEN", "APP_SEND")         : "SYN_SENT",
+    ("LISTEN", "APP_CLOSE")        : "CLOSED",
+    ("SYN_RCVD", "APP_CLOSE")      : "FIN_WAIT_1",
+    ("SYN_RCVD", "RCV_ACK")        : "ESTABLISHED",
+    ("SYN_SENT", "RCV_SYN")        : "SYN_RCVD",
+    ("SYN_SENT", "RCV_SYN_ACK")    : "ESTABLISHED",
+    ("SYN_SENT", "APP_CLOSE")      : "CLOSED",
+    ("ESTABLISHED", "APP_CLOSE")   : "FIN_WAIT_1",
+    ("ESTABLISHED", "RCV_FIN")     : "CLOSE_WAIT",
+    ("FIN_WAIT_1", "RCV_FIN")      : "CLOSING",
+    ("FIN_WAIT_1", "RCV_FIN_ACK")  : "TIME_WAIT",
+    ("FIN_WAIT_1", "RCV_ACK")      : "FIN_WAIT_2",
+    ("CLOSING", "RCV_ACK")         : "TIME_WAIT",
+    ("FIN_WAIT_2", "RCV_FIN")      : "TIME_WAIT",
+    ("TIME_WAIT", "APP_TIMEOUT")   : "CLOSED",
+    ("CLOSE_WAIT", "APP_CLOSE")    : "LAST_ACK",
+    ("LAST_ACK", "RCV_ACK")        : "CLOSED",
+}
+
+def traverse_TCP_states(events):
+    state = "CLOSED"  # initial state, always
+    for event in events:
+        action = (state, event)
+        if action in automata:
+            state = automata[action]
+        else:
+            return "ERROR"
+    return state
