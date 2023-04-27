6eba3baab89caae65531e4d0f6f2867306c198b9
iuewcqm <capricorn.k@yandex.ru>
2023-04-28 01:29:21 +0300

    ok

--- 4kyu/find_all_possible_number_combos.py
+++ 4kyu/find_all_possible_number_combos.py
@@ -1,15 +1,12 @@
 # https://www.codewars.com/kata/555b1890a75b930e63000023
 
+def sub_arr(n, m=1):
+    for i in range(m, n//2+1):
+        for j in sub_arr(n-i, i):
+            yield [i] + j
+    yield [n]
+    
 def combos(n):
-    used = []
-    def sub_arr(n, comb=[]):
-        for i in range(1, n):
-            yield from sub_arr(n-i, comb+[i])
-        comb.append(n)
-        comb.sort()
-        if comb not in used:
-            used.append(comb)
-            yield comb
     return list(sub_arr(n))
 
 if __name__ == '__main__':
